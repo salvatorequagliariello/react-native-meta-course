c194c59eecd632135ff9c01240cfe60ff08ffb64
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-19 13:23:01 +0200

    add footer component

--- first-app/first-mobile-app/App.js
+++ first-app/first-mobile-app/App.js
@@ -1,14 +1,17 @@
 import { View, Text } from "react-native";
+import LittleLemonFooter from "./components/LittleLemonFooter";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 
 
 export default function App() {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <LittleLemonHeader />
-      <Text style={{fontWeight: 700, fontSize: 30}}>
-        Ciao
-      </Text>
+    <View style={{flex: 1, justifyContent: "flex-start", alignItems: "center", backgroundColor: "#495E57", position: "relative"}}>
+      <View style={{}}>
+        <LittleLemonHeader />
+      </View>
+      <View style={{position: "absolute", bottom: 0, width: "100%"}}>
+        <LittleLemonFooter />
+      </View>
     </View>
   )
 }
--- first-app/first-mobile-app/components/LittleLemonHeader.js
+++ first-app/first-mobile-app/components/LittleLemonHeader.js
@@ -2,8 +2,8 @@ import { View, Text } from "react-native"
 
 const LittleLemonHeader = () => {
     return (
-        <View>
-            <Text>Little Lemon Restaurant</Text>
+        <View style={{backgroundColor: '#F4CE14'}}>
+            <Text style={{width: "100%", padding: 50, paddingTop: 70, paddingBottom: 0, fontSize: 30, color: "black"}}>Little Lemon Restaurant</Text>
         </View>
     )
 };
