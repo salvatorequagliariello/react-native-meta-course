815184fb8f555d2756e0b74f140433f23a24280a
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-19 19:37:51 +0200

    add stylesheet support

--- first-app/first-mobile-app/components/MenuItems.js
+++ first-app/first-mobile-app/components/MenuItems.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
 
 
-const items = ["Hummus", "Moutabal", "Falafel", "Marinated Olives", "Kofta", "Eggplant", "Salad", "lentil Burger"];
+const items = ["Hummus", "Moutabal", "Falafel", "Marinated Olives", "Kofta", "Eggplant", "Salad", "Lentil Burger"];
 
 const MenuItems = () => {
     return (
-        <View style={{flex: 1}}>
-            <Text style={{color: "white", fontSize: 20, marginLeft: 10, marginBottom: 10}}>Our Menu</Text>
-            <ScrollView  style={{backgroundColor: "#4C6960", borderRadius: 20}}>
-                <View style={{paddingVertical: 10, paddingHorizontal: 20}}>
+        <View style={menuStyle.container}>
+            <Text style={menuStyle.title}>Our Menu</Text>
+            <ScrollView style={menuStyle.scroll}>
+                <View style={menuStyle.scrollContainer}>
                     {items.map(item => {
                         return (
-                            <View style={{ marginVertical: 5, backgroundColor: "#63877D", paddingVertical: 7, paddingLeft: 10, borderRadius: 10}}>
-                        <Text style={{fontSize: 20, color: "white"}}>
+                            <View style={menuStyle.itemContainer}>
+                        <Text style={menuStyle.textStyle}>
                             {item}
                         </Text>
                     </View>
@@ -23,5 +23,36 @@ const MenuItems = () => {
         </View>
     )
 };
+
+const menuStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        color: "white", 
+        fontSize: 20, 
+        marginLeft: 10, 
+        marginBottom: 10,
+    },
+    scroll: {
+        backgroundColor: "#4C6960", 
+        borderRadius: 20
+    },
+    scrollContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 20
+    },
+    itemContainer: {
+        marginVertical: 5, 
+        backgroundColor: "#63877D", 
+        paddingVertical: 7, 
+        paddingLeft: 10, 
+        borderRadius: 10
+    },
+    textStyle: {
+        fontSize: 20, 
+        color: "white"
+    }
+});
 
 export default MenuItems;
