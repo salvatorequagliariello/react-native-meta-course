a7b803f8ae418e1523d26cbc428880552a9063ca
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-19 16:25:25 +0200

    add main content component

--- first-app/first-mobile-app/App.js
+++ first-app/first-mobile-app/App.js
@@ -1,13 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import LittleLemonHeader from "./components/LittleLemonHeader";
+import MainContent from "./components/MainContent";
 
 
 export default function App() {
   return (
     <View style={{flex: 1, justifyContent: "flex-start", alignItems: "center", backgroundColor: "#495E57", position: "relative"}}>
-      <View style={{}}>
+      <View style={{width: "100%"}}>
         <LittleLemonHeader />
+      </View>
+      <View>
+        <MainContent />
       </View>
       <View style={{position: "absolute", bottom: 0, width: "100%"}}>
         <LittleLemonFooter />
@@ -15,3 +19,4 @@ export default function App() {
     </View>
   )
 }
+ 
--- first-app/first-mobile-app/components/LittleLemonHeader.js
+++ first-app/first-mobile-app/components/LittleLemonHeader.js
@@ -3,7 +3,10 @@ import { View, Text } from "react-native"
 const LittleLemonHeader = () => {
     return (
         <View style={{backgroundColor: '#F4CE14'}}>
-            <Text style={{width: "100%", padding: 50, paddingTop: 70, paddingBottom: 0, fontSize: 30, color: "black"}}>Little Lemon Restaurant</Text>
+            <Text style={{paddingTop: 70, paddingBottom: 10, fontSize: 30, color: "black", textAlign: "center"}}>
+                <Text style={{fontWeight: 800}}>Little Lemon </Text>
+                Restaurant
+            </Text>
         </View>
     )
 };
