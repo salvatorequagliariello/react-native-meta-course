9e3037dd480e61a759ff6d9173351f5b17714384
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-20 18:36:39 +0200

    add menuitems component

---
+++ native-lists/components/MenuItems.js
@@ -0,0 +1,21 @@
+import { FlatList, Text, View } from "react-native";
+
+const list = [
+    {name: "Hummus", id: "1A"},
+    {name: "Mautabal", id: "2B"},
+    {name: "Falafel", id: "3C"},
+    {name: "Marinated Olives", id: "4D"},
+    {name: "Kofta", id: "5E"},
+    {name: "Eggplant Salad", id: "6F"},
+    {name: "Lentil Burger", id: "7G"},
+    {name: "Smoked Salmon", id: "8H"},
+    {name: "Kafta Burger", id: "9I"},
+];
+
+const MenuItems = () => {
+    return (
+        
+    );
+};  
+
+export default MenuItems;
