5e39b93939068d043352917861b6e22982d49543
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-20 20:45:35 +0200

    add sectionlist support

--- native-lists/App.js
+++ native-lists/App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,  ScrollView } from 'react-native';
 import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headertext}>Little Lemon</Text>
-      </View>
-      <MenuItems style={styles.menu} />
+        <View style={styles.headerContainer}>
+          <Text style={styles.headertext}>Little Lemon</Text>
+        </View>
+      <ScrollView style={styles.scroll}>
+        <MenuItems style={styles.menu} />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  scroll: {
+    width: "100%",
+    paddingLeft: 15
+  },
   container: {
     flex: 1,
     backgroundColor: '#495E57',
@@ -34,5 +40,7 @@ const styles = StyleSheet.create({
   },
   menu: {
     width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
--- native-lists/components/MenuItems.js
+++ native-lists/components/MenuItems.js
@@ -1,27 +1,37 @@
-import { FlatList, Text, View, StyleSheet } from "react-native";
+import { FlatList, Text, View, StyleSheet, SectionList } from "react-native";
 
 const list = [
-    { name: 'Hummus', price: '$5.00', id: '1A' },
-    { name: 'Moutabal', price: '$5.00', id: '2B' },
-    { name: 'Falafel', price: '$7.50', id: '3C' },
-    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-    { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-    { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-    { name: 'Fries', price: '$3.00', id: '11K' },
-    { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-    { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-    { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-    { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-    { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-    { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-    { name: 'Baklava', price: '$3.00', id: '18S' },
-    { name: 'Tartufo', price: '$3.00', id: '19T' },
-    { name: 'Tiramisu', price: '$5.00', id: '20U' },
-    { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+    {
+        title: 'Appetizers',
+        data: [
+          'Hummus',
+          'Moutabal',
+          'Falafel',
+          'Marinated Olives',
+          'Kofta',
+          'Eggplant Salad',
+        ],
+      },
+      {
+        title: 'Main Dishes',
+        data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+      },
+      {
+        title: 'Sides',
+        data: [
+          'Fries',
+          'Buttered Rice',
+          'Bread Sticks',
+          'Pita Pocket',
+          'Lentil Soup',
+          'Greek Salad',
+          'Rice Pilaf',
+        ],
+      },
+      {
+        title: 'Desserts',
+        data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+      },
 ];
 
 const Item = ({name, price}) => {
@@ -34,11 +44,13 @@ const Item = ({name, price}) => {
 }
 
 const MenuItems = () => {
-    const renderItem = ({item}) => <Item name={item.name} price={item.price} />
+    const renderItem = ({item}) => <Item name={item} />;
+    const renderSectionHeader = ({section: {title}}) => <Text>{title}</Text>;
+
 
     return (
         <View style={style.listContainer}>
-            <FlatList style={style.list} data={list} keyExtractor={item => item.id} renderItem={renderItem} />
+            <SectionList style={style.list} sections={list} renderItem={renderItem} renderSectionHeader={renderSectionHeader} keyExtractor={(item, index) => item + index}/>
         </View>
     );
 };
