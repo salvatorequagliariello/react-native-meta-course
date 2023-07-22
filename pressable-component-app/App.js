56ffcf02b3a5b522250b9c800d7c8570eddd9193
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-22 10:42:06 +0200

    add onpress function

--- pressable-component-app/App.js
+++ pressable-component-app/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import LittleHeader from './components/Header';
 import MenuList from './components/MenuItems';
 
 export default function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <View style={styles.container}>
       <LittleHeader />
-      <MenuList />
+      <Pressable style={styles.button} onPress={() => setShowMenu(!showMenu)}>
+        {!showMenu && <Text style={styles.buttonText}>Show menu</Text>}
+        {showMenu && <Text style={styles.buttonText}>Home</Text>}
+      </Pressable>
+      {showMenu && <MenuList />}
       <StatusBar style="auto" />
     </View>
   );
@@ -19,4 +26,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
   },
+  button: {
+    width: "40%",
+    backgroundColor: "#333333",
+    paddingVertical: 10,
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "white"
+  }
+
 });
