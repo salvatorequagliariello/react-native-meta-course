77c65c650f44a1bf3ec3dee8a5540cb9fa05a874
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-22 11:11:27 +0200

    add home component

--- pressable-component-app/App.js
+++ pressable-component-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Image } from 'react-native';
 import LittleHeader from './components/Header';
 import MenuList from './components/MenuItems';
 
@@ -14,6 +14,7 @@ export default function App() {
         {!showMenu && <Text style={styles.buttonText}>Show menu</Text>}
         {showMenu && <Text style={styles.buttonText}>Home</Text>}
       </Pressable>
+      <Image source={{uri:"https://d3c33hcgiwev3.cloudfront.net/imageAssetProxy.v1/yh3S-4LcQXGO99idg4HpPg_92f701034da44942a42fee3e878fc1a1_LIttle-Lemon-Logo.png?expiry=1690156800000&hmac=Coe_7-DdepGwkelK77Sh1uM3CEE6CydpAWeTw_2Eyjk"}} />
       {showMenu && <MenuList />}
       <StatusBar style="auto" />
     </View>
@@ -30,10 +31,13 @@ const styles = StyleSheet.create({
     width: "40%",
     backgroundColor: "#333333",
     paddingVertical: 10,
-    alignItems: "center"
+    alignItems: "center",
+    borderRadius: 10
   },
   buttonText: {
-    color: "white"
+    color: "white",
+    textTransform: "uppercase",
+    letterSpacing: 1
   }
 
 });
--- pressable-component-app/components/MenuItems.js
+++ pressable-component-app/components/MenuItems.js
@@ -2,17 +2,37 @@ import { View, Text, SectionList, StyleSheet } from "react-native";
 
 const menuItems = [
     {
-      title: 'Appetizers',
-      data: [
-        'Hummus',
-        'Moutabal',
-        'Falafel',
-        'Marinated Olives',
-        'Kofta',
-        'Eggplant Salad',
-      ],
-    }
-];
+        title: 'Appetizers',
+        data: [
+          'Hummus',
+          'Moutabal',
+          'Falafel',
+          'Marinated Olives',
+          'Kofta',
+          'Eggplant Salad',
+        ],
+      },
+      {
+        title: 'Main Dishes',
+        data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+      },
+      {
+        title: 'Sides',
+        data: [
+          'Fries',
+          'Buttered Rice',
+          'Bread Sticks',
+          'Pita Pocket',
+          'Lentil Soup',
+          'Greek Salad',
+          'Rice Pilaf',
+        ],
+      },
+      {
+        title: 'Desserts',
+        data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+      },
+    ];
 
 const MenuList = () => {
     const renderItem = ({item}) => {
@@ -28,25 +48,28 @@ const MenuList = () => {
     };
 
     return (
-        <SectionList
-        sections={menuItems}
-        renderItem={renderItem}
-        renderSectionHeader={renderSection}
-        keyExtractor={(item, index) => item + index}
-        style={styles.list}>
-        </SectionList>
+        <View style={styles.container}>
+            <SectionList
+            sections={menuItems}
+            renderItem={renderItem}
+            renderSectionHeader={renderSection}
+            keyExtractor={(item, index) => item + index}
+            style={styles.list}>
+            </SectionList>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     list: {
-        width: "90%",
+        width: "100%",
     },  
     header: {
         fontSize: 19,
         textAlign: "left",
         color: "#F4CE14",
-        marginBottom: 10
+        marginBottom: 10,
+        marginTop: 20
     },
     item: {
         color: "white",
@@ -54,6 +77,11 @@ const styles = StyleSheet.create({
         backgroundColor: "#333333",
         paddingHorizontal: 15,
         paddingVertical: 5,
+    },
+    container: {
+        width: "90%",
+        paddingVertical: 10,
+        flex: 1
     }
 })
 
