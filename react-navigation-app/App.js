e27cf7ed6aba4e0f3f4c02c1d0a0d2fa5942d6dd
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-23 09:04:10 +0200

    init expo react navigator

--- react-navigation-app/App.js
+++ react-navigation-app/App.js
@@ -1,5 +1,7 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
