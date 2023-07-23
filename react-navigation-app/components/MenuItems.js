b390a26c1d7612396626cf33731a1adb9f84f1b9
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-23 15:29:10 +0200

    add menu component

---
+++ react-navigation-app/components/MenuItems.js
@@ -0,0 +1,88 @@
+import { View, Text, SectionList, StyleSheet } from "react-native";
+
+const menuItems = [
+    {
+        title: 'Appetizers',
+        data: [
+          'Hummus',
+          'Moutabal',
+          'Falafel',
+          'Marinated Olives',
+          'Kofta',
+          'Eggplant Salad',
+        ],
+      },
+      {
+        title: 'Main Dishes',
+        data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+      },
+      {
+        title: 'Sides',
+        data: [
+          'Fries',
+          'Buttered Rice',
+          'Bread Sticks',
+          'Pita Pocket',
+          'Lentil Soup',
+          'Greek Salad',
+          'Rice Pilaf',
+        ],
+      },
+      {
+        title: 'Desserts',
+        data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+      },
+    ];
+
+const MenuList = () => {
+    const renderItem = ({item}) => {
+        return (
+                <Text style={styles.item}>{item}</Text>
+        );
+    };
+
+    const renderSection = ({section: {title}}) => {
+        return (
+                <Text style={styles.header}>{title}</Text>
+        )
+    };
+
+    return (
+        <View style={styles.container}>
+            <SectionList
+            sections={menuItems}
+            renderItem={renderItem}
+            renderSectionHeader={renderSection}
+            keyExtractor={(item, index) => item + index}
+            style={styles.list}>
+            </SectionList>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    list: {
+        width: "100%",
+    },  
+    header: {
+        fontSize: 19,
+        textAlign: "left",
+        color: "#F4CE14",
+        marginBottom: 10,
+        marginTop: 20
+    },
+    item: {
+        color: "white",
+        fontSize: 24,
+        backgroundColor: "#333333",
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+    },
+    container: {
+        width: "90%",
+        paddingVertical: 10,
+        flex: 1
+    }
+})
+
+export default MenuList;
