f1b1c6101d8ac1079958b935e4c9ae977ed7b08b
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-24 18:08:12 +0200

    add fetch request based list

--- working-with-data/App.js
+++ working-with-data/App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
 
-export default function App() {
+const renderItem = ({item}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flexDirection: "row", justifyContent:"space-between", paddingHorizontal: 20, backgroundColor: '#495E57', paddingVertical: 10}}>
+    <Text style={{fontSize: 19, color: "white"}}>{item.title}</Text>
+    <Text style={{fontSize: 19, color: "white"}}>{item.price}</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMenu = async () => {
+    try {
+      const response = await fetch("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json");
+      const json = await response.json();
+      setData(json.menu);
+    } catch (error) {
+      console.error(error);
+    } finally  {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+    }, []);
+  
+  return (
+    <SafeAreaView style={menuStyles.container}>
+      <Text style={menuStyles.headerText}>Little Lemon</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+          <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id}/> 
+      )}
+    </SafeAreaView>
+  );
+};
+
+const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#495E57',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 30,
+    marginTop: 50
   },
 });
